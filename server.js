6d7a06af664f5d6d6f529e6fc95df36966aceafe
Ubuntu <ubuntu@ip-172-31-43-3.us-east-2.compute.internal>
2018-05-12 19:52:39 +0000

    messages

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ let server = http.createServer((req, res) => {
 
 const io = require('socket.io').listen(server)
 io.sockets.on('connection', socket => {
-	socket.emit('message', 'hello from serv');
-	socket.emit('message', 'hello 2 from serv');
+	socket.on('action', action => {
+		console.log('action: ' + action);
+		socket.emit('message', 'action taken into account');
+	});
+	socket.emit('ready');
+//	socket.emit('message', 'hello from serv');
+//	socket.emit('message', 'hello 2 from serv');
 });
 server.listen(8080);
+console.log("server listens");
